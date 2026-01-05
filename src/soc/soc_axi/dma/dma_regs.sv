3e4696d7a33517c1ca6158c95db246e085622916
Bwoah-Kimi <jameszhu.20021122@gmail.com>
2026-01-05 18:41:47 +0800

    Update DMA regs

--- src/soc/soc_axi/dma/dma_regs.sv
+++ src/soc/soc_axi/dma/dma_regs.sv
@@ -41,55 +41,55 @@ module dma_regs #(
     assign launch_o   = launch_q;
 
     always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-        src_addr_lo <= '0;
-        src_addr_hi <= '0;
-        dst_addr_lo <= '0;
-        dst_addr_hi <= '0;
-        length_lo   <= '0;
-        length_hi   <= '0;
-        config_reg  <= '0;
-        launch_q    <= 1'b0;
-    end else begin
-        launch_q <= 1'b0;  // Auto-clear launch signal
+        if (!rst_ni) begin
+            src_addr_lo <= '0;
+            src_addr_hi <= '0;
+            dst_addr_lo <= '0;
+            dst_addr_hi <= '0;
+            length_lo   <= '0;
+            length_hi   <= '0;
+            config_reg  <= '0;
+            launch_q    <= 1'b0;
+        end else begin
+            launch_q <= 1'b0;  // Auto-clear launch signal
 
-        if (req_i.valid && req_i.write) begin
-        unique case (req_i.addr[7:0])
-            8'h00:   src_addr_lo <= req_i.wdata[31:0];
-            8'h08:   src_addr_hi <= req_i.wdata[31:0];
-            8'h10:   dst_addr_lo <= req_i.wdata[31:0];
-            8'h18:   dst_addr_hi <= req_i.wdata[31:0];
-            8'h20:   length_lo <= req_i.wdata[31:0];
-            8'h28:   length_hi <= req_i.wdata[31:0];
-            8'h30:   config_reg <= req_i.wdata[31:0];
-            8'h38:   launch_q <= 1'b1;
-            default: ;
-        endcase
+            if (req_i.valid && req_i.write) begin
+            unique case (req_i.addr[7:0])
+                8'h00:   src_addr_lo <= req_i.wdata[31:0];
+                8'h08:   src_addr_hi <= req_i.wdata[31:0];
+                8'h10:   dst_addr_lo <= req_i.wdata[31:0];
+                8'h18:   dst_addr_hi <= req_i.wdata[31:0];
+                8'h20:   length_lo <= req_i.wdata[31:0];
+                8'h28:   length_hi <= req_i.wdata[31:0];
+                8'h30:   config_reg <= req_i.wdata[31:0];
+                8'h38:   launch_q <= 1'b1;
+                default: ;
+            endcase
+            end
         end
-    end
     end
 
     always_comb begin
-    rsp_o.ready = 1'b1;
-    rsp_o.error = 1'b0;
-    rsp_o.rdata = '0;
+        rsp_o.ready = 1'b1;
+        rsp_o.error = 1'b0;
+        rsp_o.rdata = '0;
 
-    if (req_i.valid && !req_i.write) begin
-        unique case (req_i.addr[7:0])
-        8'h00: rsp_o.rdata = {32'b0, src_addr_lo};
-        8'h08: rsp_o.rdata = {32'b0, src_addr_hi};
-        8'h10: rsp_o.rdata = {32'b0, dst_addr_lo};
-        8'h18: rsp_o.rdata = {32'b0, dst_addr_hi};
-        8'h20: rsp_o.rdata = {32'b0, length_lo};
-        8'h28: rsp_o.rdata = {32'b0, length_hi};
-        8'h30: rsp_o.rdata = {32'b0, config_reg};
-        8'h40:
-        rsp_o.rdata = {
-            32'b0, 29'b0, ready_i, error_i, busy_i
-        };  // Status: [2]=Ready, [1]=Error, [0]=Busy
-        default: rsp_o.error = 1'b1;
-        endcase
-    end
+        if (req_i.valid && !req_i.write) begin
+            unique case (req_i.addr[7:0])
+            8'h00: rsp_o.rdata = {32'b0, src_addr_lo};
+            8'h08: rsp_o.rdata = {32'b0, src_addr_hi};
+            8'h10: rsp_o.rdata = {32'b0, dst_addr_lo};
+            8'h18: rsp_o.rdata = {32'b0, dst_addr_hi};
+            8'h20: rsp_o.rdata = {32'b0, length_lo};
+            8'h28: rsp_o.rdata = {32'b0, length_hi};
+            8'h30: rsp_o.rdata = {32'b0, config_reg};
+            8'h40:
+            rsp_o.rdata = {
+                32'b0, 29'b0, ready_i, error_i, busy_i
+            };  // Status: [2]=Ready, [1]=Error, [0]=Busy
+            default: rsp_o.error = 1'b1;
+            endcase
+        end
     end
 
 endmodule
