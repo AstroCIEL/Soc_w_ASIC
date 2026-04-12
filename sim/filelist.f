a82f370284ee7acb6c3d6b9d40dc9b13920384f5
HUJIYONG <jiyong_hu@outlook.com>
2026-04-12 10:06:32 +0000

    mv sim

--- sim/filelist.f
+++ sim/filelist.f
@@ -1,22 +1,25 @@
-// Master filelist - all paths relative to sim/
 
 // Technology
 -f ../hardware/tech/filelist_sim.f
 
 // IPs (order matters: dependencies first)
 -f ../hardware/ip/common_cells/filelist.f
+-f ../hardware/ip/obi/filelist.f
 -f ../hardware/ip/apb/filelist.f
 -f ../hardware/ip/axi/filelist.f
+-f ../hardware/ip/axi_slice/filelist.f
+-f ../hardware/ip/register_interface/filelist.f
+-f ../hardware/ip/axi_riscv_atomics/filelist.f
+-f ../hardware/ip/axi2apb/filelist.f
 -f ../hardware/ip/fpnew/filelist.f
 -f ../hardware/ip/fpu_div_sqrt_mvp/filelist.f
 -f ../hardware/ip/cva6/filelist.f
 -f ../hardware/ip/ara/filelist.f
+-f ../hardware/ip/riscv-dbg/filelist.f
+-f ../hardware/ip/clint/filelist.f
+-f ../hardware/ip/apb_timer/filelist.f
+-f ../hardware/ip/apb_uart/filelist.f
+-f ../hardware/ip/rv_plic/filelist.f
 
-// SoC
--f ../hardware/soc/filelist.f
 
-// Testbench
-../tb/mock_uart.sv
-../tb/ara_testharness.sv
-../tb/ara_tb.sv
-../tb/ara_tb_verilator.sv
+-f ../hardware/ariane_soc/fileist.f
