ce1e54baed758db59f4171b0218272e48ad9ec1e
HUJIYONG <jiyong_hu@outlook.com>
2026-04-29 15:15:07 +0000

    mv soc

---
+++ hardware/soc/filelist.f
@@ -0,0 +1,17 @@
+
+../hardware/soc/cva6_accel_first_pass_decoder.sv
+../hardware/soc/axi_inval_filter.sv
+../hardware/soc/ara_system.sv
+../hardware/soc/ctrl_registers.sv
+../hardware/soc/ariane_axi_pkg.sv
+../hardware/soc/axi_lite_interface.sv
+../hardware/soc/ariane_soc_pkg.sv
+../hardware/soc/ariane_axi_soc_pkg.sv
+../hardware/soc/ariane_peripherals.sv
+../hardware/soc/bootrom/bootrom.sv
+../hardware/soc/clint.sv
+../hardware/soc/ariane_soc_top.sv
+
+
+
+
