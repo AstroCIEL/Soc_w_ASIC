0dde137fadb0832b864ce87b6a92352ef9fc6c16
HUJIYONG <jiyong_hu@outlook.com>
2026-04-29 15:01:35 +0000

    remove  old soc

--- hardware/soc/filelist.f
+++
@@ -1,5 +0,0 @@
-../hardware/soc/src/cva6_accel_first_pass_decoder.sv
-../hardware/soc/src/axi_inval_filter.sv
-../hardware/soc/src/ctrl_registers.sv
-../hardware/soc/src/ara_system.sv
-../hardware/soc/src/ara_soc.sv
