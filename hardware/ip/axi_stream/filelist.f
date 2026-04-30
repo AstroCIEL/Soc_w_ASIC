79acc04a17e1b78260875b93d47e933aed2fbffd
HUJIYONG <jiyong_hu@outlook.com>
2026-04-30 06:43:23 +0000

    add axi_stream ip

---
+++ hardware/ip/axi_stream/filelist.f
@@ -0,0 +1,7 @@
++incdir+../hardware/ip/axi_stream/include
+
+../hardware/ip/axi_stream/src/axi_stream_multicut.sv
+../hardware/ip/axi_stream/src/axi_stream_dw_downsizer.sv
+../hardware/ip/axi_stream/src/axi_stream_cut.sv
+../hardware/ip/axi_stream/src/axi_stream_intf.sv
+../hardware/ip/axi_stream/src/axi_stream_dw_upsizer.sv
