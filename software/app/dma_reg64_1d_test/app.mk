7503bccef92bfda8b867f2a33e8a4e22df483464
AstroCIEL <782306774@qq.com>
2026-05-06 14:47:37 +0800

    software make passed on centos

--- software/app/dma_reg64_1d_test/app.mk
+++ software/app/dma_reg64_1d_test/app.mk
@@ -1,2 +1,13 @@
 # Per-app build description for the DMA reg64_1d memcpy demo.
 dma_reg64_1d_test_SRCS := $(APP_DIR_dma_reg64_1d_test)/main.c
+
+# Extra runtime objects needed for this app (DMA specific)
+dma_reg64_1d_test_EXTRA_OBJS := $(BUILD_DIR)/soc/src/dma_reg64_1d.c.o
+
+# Custom CFLAGS: use medlow code model, disable builtin printf
+dma_reg64_1d_test_CFLAGS := -mcmodel=medlow -fno-builtin-printf
+
+# Custom LDFLAGS: add --no-relax to avoid relocation issues
+dma_reg64_1d_test_LDFLAGS := -static -nostartfiles -nostdlib -Wl,--gc-sections -Wl,--no-relax \
+    -T$(SCRIPTS_DIR)/link.ld -L$(HOME)/tools/riscv/lib/gcc/riscv64-unknown-elf/13.2.0 \
+    -lm -lgcc
--- software/sdk/src/syscalls.c
+++ software/sdk/src/syscalls.c
@@ -1,396 +1,123 @@
-/* An extremely minimalist syscalls.c for newlib
- * Based on riscv newlib libgloss/riscv/sys_*.c
- *
- * Copyright 2019 Claire Wolf
- * Copyright 2019 ETH Zürich and University of Bologna
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- * PERFORMANCE OF THIS SOFTWARE.
+/* Minimal system call stubs for newlib compatibility
+ * These are required when linking against libc.a
  */
 
 #include <sys/stat.h>
-#include <sys/timeb.h>
-#include <sys/times.h>
-#include <sys/utime.h>
-#include <newlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
-#include <machine/syscall.h>
-#include <assert.h>
-#include "soc_ctrl.h"
-#include "serial.h"
-#include "context.h"
+
 #undef errno
 extern int errno;
 
-#define STDOUT_FILENO 1
-#define STDERR_FILENO 2
-
-/* It turns out that older newlib versions use different symbol names which goes
- * against newlib recommendations. Anyway this is fixed in later version.
- */
-#if __NEWLIB__ <= 2 && __NEWLIB_MINOR__ <= 5
-#define _sbrk sbrk
-#define _write write
-#define _close close
-#define _lseek lseek
-#define _read read
-#define _fstat fstat
-#define _isatty isatty
-#endif
-/* Upstream newlib now defines this in libgloss/riscv/internal_syscall.h.  */
-long
-__syscall_error(long a0)
+/* System call error stub */
+long __syscall_error(long a0)
 {
-  errno = -a0;
-  return -1;
+    errno = -a0;
+    return -1;
 }
 
-void unimplemented_syscall()
-{
-  uart0.puts(&uart0, "BSP: Unimplemented system call called!\n");
-}
-
-int nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
-{
-  (void)rqtp; (void)rmtp;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _access(const char *file, int mode)
-{
-  (void)file; (void)mode;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _chdir(const char *path)
-{
-  (void)path;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _chmod(const char *path, mode_t mode)
-{
-  (void)path; (void)mode;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _chown(const char *path, uid_t owner, gid_t group)
-{
-  (void)path; (void)owner; (void)group;
-  errno = ENOSYS;
-  return -1;
-}
-
+/* File operations stubs */
 int _close(int file)
 {
-  (void)file;
-  return -1;
-}
-
-int _execve(const char *name, char *const argv[], char *const env[])
-{
-  (void)name; (void)argv; (void)env;
-  errno = ENOMEM;
-  return -1;
-}
-
-void _exit(int exit_status)
-{
-  soc_exit(exit_status);
-}
-
-int _faccessat(int dirfd, const char *file, int mode, int flags)
-{
-  (void)dirfd; (void)file; (void)mode; (void)flags;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _fork(void)
-{
-  errno = EAGAIN;
-  return -1;
+    (void)file;
+    return -1;
 }
 
 int _fstat(int file, struct stat *st)
 {
-  (void)file;
-  st->st_mode = S_IFCHR;
-  return 0;
-  // errno = -ENOSYS;
-  // return -1;
-}
-
-int _fstatat(int dirfd, const char *file, struct stat *st, int flags)
-{
-  (void)dirfd; (void)file; (void)st; (void)flags;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _ftime(struct timeb *tp)
-{
-  (void)tp;
-  errno = ENOSYS;
-  return -1;
-}
-
-char *_getcwd(char *buf, size_t size)
-{
-  (void)buf; (void)size;
-  errno = -ENOSYS;
-  return NULL;
-}
-
-int _getpid()
-{
-  return 1;
-}
-
-int _gettimeofday(struct timeval *tp, void *tzp)
-{
-  (void)tp; (void)tzp;
-  errno = -ENOSYS;
-  return -1;
+    (void)file;
+    st->st_mode = S_IFCHR;
+    return 0;
 }
 
 int _isatty(int file)
 {
-  return (file == STDOUT_FILENO) || (file == STDERR_FILENO);
-}
-
-int _kill(int pid, int sig)
-{
-  (void)pid; (void)sig;
-  errno = EINVAL;
-  return -1;
-}
-
-int _link(const char *old_name, const char *new_name)
-{
-  (void)old_name; (void)new_name;
-  errno = EMLINK;
-  return -1;
+    (void)file;
+    return 1;
 }
 
 off_t _lseek(int file, off_t ptr, int dir)
 {
-  (void)file; (void)ptr; (void)dir;
-  return 0;
-}
-
-int _lstat(const char *file, struct stat *st)
-{
-  (void)file; (void)st;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _open(const char *name, int flags, int mode)
-{
-  (void)name; (void)flags; (void)mode;
-  return -1;
-}
-
-int _openat(int dirfd, const char *name, int flags, int mode)
-{
-  (void)dirfd; (void)name; (void)flags; (void)mode;
-  errno = ENOSYS;
-  return -1;
+    (void)file;
+    (void)ptr;
+    (void)dir;
+    return 0;
 }
 
 ssize_t _read(int file, void *ptr, size_t len)
 {
-  (void)file; (void)ptr; (void)len;
-  return 0;
-}
-
-int _stat(const char *file, struct stat *st)
-{
-  (void)file;
-  st->st_mode = S_IFCHR;
-  return 0;
-  // errno = ENOSYS;
-  // return -1;
-}
-
-long _sysconf(int name)
-{
-  (void)name;
-  return -1;
-}
-
-clock_t _times(struct tms *buf)
-{
-  (void)buf;
-  return -1;
-}
-
-int _unlink(const char *name)
-{
-  (void)name;
-  errno = ENOENT;
-  return -1;
-}
-
-int _utime(const char *path, const struct utimbuf *times)
-{
-  (void)path; (void)times;
-  errno = ENOSYS;
-  return -1;
-}
-
-int _wait(int *status)
-{
-  (void)status;
-  errno = ECHILD;
-  return -1;
+    (void)file;
+    (void)ptr;
+    (void)len;
+    return 0;
 }
 
 ssize_t _write(int file, const void *ptr, size_t len)
 {
-  const char *cptr = (char *)ptr;
-  if (file != STDOUT_FILENO && file != STDERR_FILENO)
-    {
-      errno = ENOSYS;
-      return -1;
-    }
-
-  const void *eptr = cptr + len;
-  while (cptr != eptr)
-    uart0.putc(&uart0, *cptr++);
-  return len;
+    (void)file;
+    (void)ptr;
+    (void)len;
+    return len;
 }
 
-extern char __heap_start[];
-extern char __heap_end[];
-static char *brk = __heap_start;
-
-int _brk(void *addr)
-{
-  brk = addr;
-  return 0;
-}
-
+/* Memory allocation stub */
 void *_sbrk(ptrdiff_t incr)
 {
-  char *old_brk = brk;
-  long sp;
-  __asm__ volatile ("mv %0, sp" : "=r"(sp));
+    extern char __heap_start[];
+    extern char __heap_end[];
+    static char *heap_ptr = NULL;
 
-  char *new_brk = brk += incr;
-  if (new_brk < (char *) sp && new_brk < __heap_end)
-    {
-      brk = new_brk;
+    if (heap_ptr == NULL)
+        heap_ptr = __heap_start;
 
-      return old_brk;
+    char *new_ptr = heap_ptr + incr;
+    if (new_ptr > __heap_end) {
+        errno = ENOMEM;
+        return (void *)-1;
     }
-  else
-    {
-      errno = ENOMEM;
-      return (void *) -1;
-    }
+
+    char *old_ptr = heap_ptr;
+    heap_ptr = new_ptr;
+    return old_ptr;
 }
 
-void handle_syscall(trap_context_t *ctx)
+/* Exit stub - loop forever */
+void _exit(int status)
 {
-  long a0 = (long)ctx->gpr[REG_A0];
-  long a1 = (long)ctx->gpr[REG_A1];
-  long a2 = (long)ctx->gpr[REG_A2];
-  long a3 = (long)ctx->gpr[REG_A3];
-  long syscall_id = (long)ctx->gpr[REG_A7];
-
-  long ret = -ENOSYS;
-  switch (syscall_id) {
-    case SYS_exit:
-      _exit(a0);
-      __builtin_unreachable();
-    case SYS_read:
-      ret = _read(a0, (void *)a1, a2);
-      break;
-    case SYS_write:
-      ret = _write(a0, (const void *)a1, a2);
-      break;
-    case SYS_getpid:
-      ret = _getpid();
-      break;
-    case SYS_kill:
-      ret = _kill(a0, a1);
-      break;
-    case SYS_open:
-      ret = _open((const char *)a0, a1, a2);
-      break;
-    case SYS_openat:
-      ret = _openat(a0, (const char *)a1, a2, a3);
-      break;
-    case SYS_close:
-      ret = _close(a0);
-      break;
-    case SYS_lseek:
-      ret = _lseek(a0, a1, a2);
-      break;
-    case SYS_brk:
-      ret = _brk((void *)a0);
-      break;
-    case SYS_link:
-      ret = _link((const char *)a0, (const char *)a1);
-      break;
-    case SYS_unlink:
-      ret = _unlink((const char *)a0);
-      break;
-    case SYS_chdir:
-      ret = _chdir((const char *)a0);
-      break;
-    case SYS_getcwd:
-      ret = (long)_getcwd((char *)a0, a1);
-      break;
-    case SYS_stat:
-      ret = _stat((const char *)a0, (struct stat *)a1);
-      break;
-    case SYS_fstat:
-      ret = _fstat(a0, (struct stat *)a1);
-      break;
-    case SYS_lstat:
-      ret = _lstat((const char *)a0, (struct stat *)a1);
-      break;
-    case SYS_fstatat:
-      ret = _fstatat(a0, (const char *)a1, (struct stat *)a2, a3);
-      break;
-    case SYS_access:
-      ret = _access((const char *)a0, a1);
-      break;
-    case SYS_faccessat:
-      ret = _faccessat(a0, (const char *)a1, a2, a3);
-      break;
-    case SYS_gettimeofday:
-      ret = _gettimeofday((struct timeval *)a0, (void *)a1);
-      break;
-    case SYS_times:
-      ret = _times((struct tms *)a0);
-      break;
-    default:
-      unimplemented_syscall();
-      break;
-  }
-  ctx->gpr[REG_A0] = (uint64_t)ret;
+    (void)status;
+    while (1);
 }
 
-void ctx_advance_mepc(trap_context_t *ctx)
+/* puts stub - use printf's _putchar via uart */
+extern void _putchar(char c);
+int puts(const char *s)
 {
-  uint16_t hw = *(const uint16_t *)(uintptr_t)ctx->mepc;
-  ctx->mepc += ((hw & 0x3) == 0x3) ? 4 : 2;
+    while (*s) {
+        _putchar(*s++);
+    }
+    _putchar('\n');
+    return 0;
+}
+
+/* putchar stub */
+int putchar(int c)
+{
+    _putchar((char)c);
+    return c;
+}
+
+/* Get PID stub */
+int _getpid(void)
+{
+    return 1;
+}
+
+/* Kill stub */
+int _kill(int pid, int sig)
+{
+    (void)pid;
+    (void)sig;
+    errno = EINVAL;
+    return -1;
 }
