be32041d7d9bb86ec7cae1c65ba0b242331e7c24
ziaoxiao <ziaoxiao@stu.pku.edu.cn>
2026-07-19 10:14:13 +0800

    修改综合filelist.f

---
+++ hardware/user_ip/dcim_wrap/filelist/top/filelist_syn.f
@@ -0,0 +1,13 @@
+// Top Wrapper
+// -F ../../filelist_dcim_wrap.f
+../../dcim_wrap.sv
+
+// RegisterFile IP & Wrapper
+-F ../../macro/filelist_syn.f
+
+// Common RTL
+-F ../../common/filelist.f
+
+// Adapt Layer Between AXI & DCIM
+-F ../../adapt/filelist.f
+
